c0cb206822cbfa0dd7a4f694080dd34ba55057c6
khoi.tla <khoi.tla>
2024-12-29 19:31:35 +0700

    Xác thực tài khoản

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '1111',
+      database: 'work_planner_db',
+      entities: [],
+      synchronize: true,
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { AuthPayloadDto } from './dto/auth.dto';
+import { JwtService } from '@nestjs/jwt';
+
+const fakeUsers = [
+  { id: 1, username: 'khoi', password: '1111' },
+  { id: 2, username: 'linh', password: '2222' },
+  { id: 3, username: 'khoi2', password: '3333' },
+];
+
+@Injectable()
+export class AuthService {
+  constructor(private jwtService: JwtService) {}
+
+  validateUser({ username, password }: AuthPayloadDto) {
+    const findUser = fakeUsers.find((user) => user.username === username);
+    if (!findUser) return null;
+    if (password === findUser.password) {
+      const { password, ...user } = findUser;
+      return this.jwtService.sign(user);
+    }
+  }
+}
